28d285fd0cb1424d99b44a207496fcd7fa491bcc
Milena <jessicaa.milena@gmail.com>
2022-02-16 13:54:16 -0400

    adding more lists

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const res = require("express/lib/response");
-
+const date = require(__dirname + "date.js")
 const app = express();
 
 let items = ["Buy food", "Cook", "Have dinner", "Play"];
 let workItems = [];
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
+//----------- Diary list ------------
+
 app.get("/", function(req, res){
 
-    let today = new Date();
-    
-    let options = {
-        weekday : "long",
-        day : "numeric",
-        month : "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {ListTitle: day, newListItems: items});
+    let day = date.getDate();
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-    let item = req.body.newItem;
-    items.push(item);
 
-    res.redirect("/");
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+   
 });
 
-app.get("/work", function(){
-    res.render("list", {ListTitle: "Work List", newListItems: workItems})
-})
+// -----------Work List -------------
+
+app.get("/work", (req, res) => {
+        res.render("list", {listTitle: "Work List", newListItems: workItems });
+    });
 
 app.post("/work", function(req, res){
     let item = req.body.newItem;
+    workItems.push(item);
     res.redirect("/work")
 })
 
